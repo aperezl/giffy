aeb11b3cc6ff6731c1d1d59e5c45bbc611342cc7
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-03-15 17:19:31 +0100

    add css to Gif component

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Gif.css'
 
 const Gif = ({ id, title, url}) => {
   return (
-    <div key={id}>
+    <div key={id} className="Gif">
       <small>{id}</small>
       <h4>{title}</h4>
       <img alt={title}  src={url} />
